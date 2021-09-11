594b2628b540a41c9b3a011e2e56ba4c173d1866
daring-dayana <88841212+daring-dayana@users.noreply.github.com>
2021-09-11 11:21:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 var garden,rabbit;
 var gardenImg,rabbitImg;
-
+var apple, appleImg
+var leaf, leafImg
 function preload(){
   gardenImg = loadImage("garden.png");
   rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png")
+  leafImg = loadImage("leaf.png")
 }
 
 function setup(){
@@ -26,6 +29,35 @@ function draw() {
   
   edges= createEdgeSprites();
   rabbit.collide(edges);
-
+  rabbit.x=World.mouseX
+  var select_sprites = Math.round(random(1,3));
+  if (frameCount % 80 == 0) {
+  if (select_sprites  == 1) 
+  { createApples(); }
+  else if  (select_sprites == 2) {         createApples(); }
+else {  createApples(); } }
   drawSprites();
+  
+  var select_sprites = Math.round(random(1,3));
+  if (frameCount % 100 == 0) {
+  if (select_sprites  == 1) 
+  { createLeaves(); }
+  else if  (select_sprites == 5) {
+    createLeaves(); }
+else {  createLeaves(); } }
+  drawSprites();
+}
+function createApples() {
+apple=createSprite(random(50,350),40,10,10)
+apple.addImage(appleImg);
+apple.scale=0.1;
+apple.velocityY=5;
+apple.lifetime=150;
+}
+function createLeaves() {
+leaf=createSprite(random(50,350),40,10,10)
+leaf.addImage(leafImg);
+leaf.scale=0.1;
+leaf.velocityY=5;
+leaf.lifetime=150;
 }
